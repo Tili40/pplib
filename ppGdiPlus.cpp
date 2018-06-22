ac22daca435e1ead63cc8082f0176e3fecd57ebe
podoroges <pavel.krents@gmail.com>
2018-06-22 11:13:00 +0300

    v0.05

---
+++ ppGdiPlus.cpp
@@ -0,0 +1,228 @@
+#include "ppgdiplus.h"
+#pragma link "gdiplus.lib"
+#include <algorithm>
+using std::min;
+using std::max;
+#include <classes.hpp>
+#include <wingdi.h> // RGB
+#include <gdiplus.h>
+
+/*
+void DrawRoundRectangle(Gdiplus::Graphics* g, Gdiplus::Pen *p, Gdiplus::Rect& rect, int radius)
+{
+	if (g==NULL) return;
+	Gdiplus::GraphicsPath path;
+
+	path.AddLine(rect.X + radius, rect.Y, rect.X + rect.Width - (radius * 2), rect.Y);
+	path.AddArc(rect.X + rect.Width - (radius * 2), rect.Y, radius * 2, radius * 2, 270, 90);
+	path.AddLine(rect.X + rect.Width, rect.Y + radius, rect.X + rect.Width, rect.Y + rect.Height - (radius * 2));
+	path.AddArc(rect.X + rect.Width - (radius * 2), rect.Y + rect.Height - (radius * 2), radius * 2,
+		radius * 2, 0, 90);
+	path.AddLine(rect.X + rect.Width - (radius * 2), rect.Y + rect.Height, rect.X + radius, rect.Y + rect.Height);
+	path.AddArc(rect.X, rect.Y + rect.Height - (radius * 2), radius * 2, radius * 2, 90, 90);
+	path.AddLine(rect.X, rect.Y + rect.Height - (radius * 2), rect.X, rect.Y + radius);
+	path.AddArc(rect.X, rect.Y, radius * 2, radius * 2, 180, 90);
+	path.CloseFigure();
+	g->DrawPath(p, &path);
+}
+
+void FillRoundRectangle(Gdiplus::Graphics* g, Gdiplus::Brush *p, Gdiplus::Rect& rect, int radius)
+{
+	if (g == NULL) return;
+	Gdiplus::GraphicsPath path;
+
+	path.AddLine(rect.X + radius, rect.Y, rect.X + rect.Width - (radius * 2), rect.Y);
+	path.AddArc(rect.X + rect.Width - (radius * 2), rect.Y, radius * 2, radius * 2, 270, 90);
+	path.AddLine(rect.X + rect.Width, rect.Y + radius, rect.X + rect.Width, rect.Y + rect.Height - (radius * 2));
+	path.AddArc(rect.X + rect.Width - (radius * 2), rect.Y + rect.Height - (radius * 2), radius * 2, radius * 2, 0, 90);
+	path.AddLine(rect.X + rect.Width - (radius * 2), rect.Y + rect.Height, rect.X + radius, rect.Y + rect.Height);
+	path.AddArc(rect.X, rect.Y + rect.Height - (radius * 2), radius * 2, radius * 2, 90, 90);
+	path.AddLine(rect.X, rect.Y + rect.Height - (radius * 2), rect.X, rect.Y + radius);
+	path.AddArc(rect.X, rect.Y, radius * 2, radius * 2, 180, 90);
+	path.CloseFigure();
+
+	g->FillPath(p, &path);
+}
+
+
+
+*/
+
+// Two variables required for startup
+Gdiplus::GdiplusStartupInput gdiplusStartupInput;
+ULONG_PTR  gdiplusToken;
+
+Gdiplus::Bitmap * pbmp; // Internal bitmap
+
+void * ppGdiPlus::GetInternalBitmap(){
+  return (void *)pbmp;
+}
+
+
+// Constructor
+ppGdiPlus::ppGdiPlus(int Width,int Height){
+  pWidth = Width;
+  pHeight = Height;
+  GdiplusStartup(&gdiplusToken, &gdiplusStartupInput, NULL);
+  pbmp = new Gdiplus::Bitmap(pWidth,pHeight);
+  pPenColor = clBlack;
+  pPenWidth = 1;
+  FontSize = 10;
+  pAlpha = 0;
+}
+// Destructor
+ppGdiPlus::~ppGdiPlus(){
+  delete pbmp;
+  Gdiplus::GdiplusShutdown(gdiplusToken);
+}
+// Resize
+void ppGdiPlus::pResize(){
+  delete pbmp;
+  pbmp = new Gdiplus::Bitmap(pWidth,pHeight);
+}
+void ppGdiPlus::Resize(int w,int h){
+  pWidth = w;
+  pHeight = h;
+  pResize();
+}
+
+int __fastcall ppGdiPlus::pGetWidth(){
+  return pWidth;
+}
+int __fastcall ppGdiPlus::pGetHeight(){
+  return pHeight;
+}
+void __fastcall ppGdiPlus::pSetWidth(int i){
+  pWidth = i;
+}
+void __fastcall ppGdiPlus::pSetHeight(int i){
+  pHeight = i;
+}
+// PenColor
+TColor __fastcall ppGdiPlus::pGetPenColor(){
+  return pPenColor;
+}
+void __fastcall ppGdiPlus::pSetPenColor(TColor c){
+  pPenColor = c;
+}
+// PenWidth
+double __fastcall ppGdiPlus::pGetPenWidth(){
+  return pPenWidth;
+}
+void __fastcall ppGdiPlus::pSetPenWidth(double d){
+  pPenWidth = d;
+}
+// BrushColor
+TColor __fastcall ppGdiPlus::pGetBrushColor(){
+  return pBrushColor;
+}
+void __fastcall ppGdiPlus::pSetBrushColor(TColor c){
+  pBrushColor = c;
+}
+// Alpha
+double __fastcall ppGdiPlus::pGetAlpha(){
+  return pAlpha;
+}
+void __fastcall ppGdiPlus::pSetAlpha(double d){
+  pAlpha = d;
+}
+// FontSize
+int __fastcall ppGdiPlus::pGetFontSize(){
+  return pFontSize;
+}
+void __fastcall ppGdiPlus::pSetFontSize(int s){
+  pFontSize = s;
+}
+
+// Line
+void ppGdiPlus::Line(int x1,int y1,int x2,int y2){
+  Gdiplus::Graphics g(pbmp);
+  Gdiplus::Pen pen(Gdiplus::Color(GetRValue(pPenColor), GetGValue(pPenColor), GetBValue(pPenColor)),(float)pPenWidth);
+  g.SetSmoothingMode(Gdiplus::SmoothingModeAntiAlias);
+  g.DrawLine(&pen, x1, y1, x2, y2);
+}
+// Draw
+void ppGdiPlus::Draw(TCanvas * c,int x,int y){
+  Gdiplus::Graphics g2(c->Handle);
+  g2.DrawImage(pbmp,x,y);
+}
+// Clear
+void ppGdiPlus::Clear(TColor c){
+  Gdiplus::Graphics g(pbmp);
+  g.Clear(Gdiplus::Color(GetRValue(c), GetGValue(c), GetBValue(c)));
+}
+// Rect
+void ppGdiPlus::Rect(int x1,int y1,int x2,int y2){
+  Gdiplus::Graphics g(pbmp);
+  Gdiplus::Pen pen(Gdiplus::Color(GetRValue(pPenColor), GetGValue(pPenColor), GetBValue(pPenColor)),pPenWidth);
+  g.DrawRectangle(&pen, x1, y1, x2-x1, y2-y1);
+}
+// FillRect
+void ppGdiPlus::FillRect(int x1,int y1,int x2,int y2){
+  Gdiplus::Graphics g(pbmp);
+  Gdiplus::SolidBrush brush(Gdiplus::Color(pAlpha,GetRValue(pBrushColor), GetGValue(pBrushColor), GetBValue(pBrushColor)));
+//  Gdiplus::SolidBrush brush(Gdiplus::Color(GetRValue(pBrushColor), GetGValue(pBrushColor), GetBValue(pBrushColor)));
+  g.FillRectangle(&brush, x1, y1, x2-x1, y2-y1);
+}
+// DrawEllipse
+void ppGdiPlus::DrawEllipse(int x1,int y1,int x2,int y2){
+  Gdiplus::Graphics g(pbmp);
+  g.SetSmoothingMode(Gdiplus::SmoothingModeAntiAlias);
+  Gdiplus::Pen pen(Gdiplus::Color(GetRValue(pPenColor), GetGValue(pPenColor), GetBValue(pPenColor)),pPenWidth);
+  g.DrawEllipse(&pen, x1, y1, x2-x1, y2-y1);
+}
+// FillEllipse
+void ppGdiPlus::FillEllipse(int x1,int y1,int x2,int y2){
+  Gdiplus::Graphics g(pbmp);
+  g.SetSmoothingMode(Gdiplus::SmoothingModeAntiAlias);
+  Gdiplus::SolidBrush brush(Gdiplus::Color(GetRValue(pBrushColor), GetGValue(pBrushColor), GetBValue(pBrushColor)));
+  g.FillEllipse(&brush, x1, y1, x2-x1, x2-x1);
+}
+// DrawCircle
+void ppGdiPlus::DrawCircle(double x1,double y1,double r){
+  Gdiplus::Graphics g(pbmp);
+  g.SetSmoothingMode(Gdiplus::SmoothingModeAntiAlias);
+  Gdiplus::Pen pen(Gdiplus::Color(GetRValue(pPenColor), GetGValue(pPenColor), GetBValue(pPenColor)),pPenWidth);
+  g.DrawEllipse(&pen, float(x1-r), float(y1-r), float(2*r), float(2*r));
+}
+// FillCircle
+void ppGdiPlus::FillCircle(double x1,double y1,double r){
+  Gdiplus::Graphics g(pbmp);
+  Gdiplus::SolidBrush brush(Gdiplus::Color(pAlpha,GetRValue(pBrushColor), GetGValue(pBrushColor), GetBValue(pBrushColor)));
+  g.FillEllipse(&brush, float(x1-r), float(y1-r), float(2*r), float(2*r));
+}
+// Polyline
+void ppGdiPlus::Polyline(TPoint * p,int cnt){
+  Gdiplus::Graphics g(pbmp);
+  g.SetSmoothingMode(Gdiplus::SmoothingModeAntiAlias);
+  Gdiplus::Pen pen(Gdiplus::Color(GetRValue(pPenColor), GetGValue(pPenColor), GetBValue(pPenColor)),pPenWidth);
+  g.DrawPolygon(&pen, (Gdiplus::Point*)p ,cnt);
+}
+// Polygon
+void ppGdiPlus::Polygon(TPoint * p,int cnt){
+  Gdiplus::Graphics g(pbmp);
+  g.SetSmoothingMode(Gdiplus::SmoothingModeAntiAlias);
+  Gdiplus::SolidBrush brush(Gdiplus::Color(pAlpha,GetRValue(pBrushColor), GetGValue(pBrushColor), GetBValue(pBrushColor)));
+  g.FillPolygon(&brush, (Gdiplus::Point*)p ,cnt);
+}
+// DrawImage
+void ppGdiPlus::DrawImage(void * image,float x1,float y1,float x2,float y2){
+  Gdiplus::Graphics g(pbmp);
+  g.DrawImage((Gdiplus::Bitmap *)image,x1,y1,x2,y2);
+}
+// TextOut
+void ppGdiPlus::TextOut(double x,double y,AnsiString st){
+  WideString wst = WideString(st);
+  Gdiplus::Graphics g(pbmp);
+  Gdiplus::SolidBrush brush(Gdiplus::Color(pAlpha,GetRValue(pBrushColor), GetGValue(pBrushColor), GetBValue(pBrushColor)));
+  Gdiplus::Font font(&Gdiplus::FontFamily(L"Arial"), pFontSize);
+  Gdiplus::PointF p(x,y);
+  g.DrawString((WCHAR *)wst.c_bstr(),wst.Length(),&font,p,&brush);
+}
+
+
+
+
+
+
+
---
+++ ppGdiPlus.h
@@ -0,0 +1,69 @@
+/*
+  ppGdiPlus - GdiPlus wrapper for BCB 6.0 by Podoroges
+  v0.05 22/06/2018 - Added GetInternalBitmap
+  v0.04 12/06/2018 - Added TextOut, FontSize
+  v0.03 23/05/2018 - Added DrawCircle, FillCircle, Alpha
+  v0.02 22/05/2018 - Added Polygon, Polyline, FillEllipse
+  v0.01 30/03/2018 - Start of the project
+
+  In case of following error:
+  [C++ Error] GdiplusGraphics.h(34): E2015 Ambiguity between
+  'Gdiplus::Graphics::Graphics(void *)' and 'Gdiplus::Graphics::Graphics(void *,int)'
+  FIX: add ";STRICT" into Project->Options->Conditional->Conditional defines
+*/
+#include <graphics.hpp> // TColor
+
+class ppGdiPlus{
+private:
+  int pWidth;
+  int __fastcall pGetWidth();
+  void __fastcall pSetWidth(int);
+  int pHeight;
+  int __fastcall pGetHeight();
+  void __fastcall pSetHeight(int);
+  void pResize();
+  TColor pPenColor;
+  TColor __fastcall pGetPenColor();
+  void __fastcall pSetPenColor(TColor);
+  double pPenWidth;
+  double __fastcall pGetPenWidth();
+  void __fastcall pSetPenWidth(double);
+  TColor pBrushColor;
+  TColor __fastcall pGetBrushColor();
+  void __fastcall pSetBrushColor(TColor);
+  double pAlpha;
+  double __fastcall pGetAlpha();
+  void __fastcall pSetAlpha(double);
+  int pFontSize;
+  int __fastcall pGetFontSize();
+  void __fastcall pSetFontSize(int);
+
+public:
+
+  __property int Width = {read=pGetWidth,write=pSetWidth};
+  __property int Height = {read=pGetHeight,write=pSetHeight};
+  void Resize(int,int);
+  __property TColor PenColor = {read=pGetPenColor,write=pSetPenColor};
+  __property double PenWidth = {read=pGetPenWidth,write=pSetPenWidth};
+  __property TColor BrushColor = {read=pGetBrushColor,write=pSetBrushColor};
+  __property double Alpha = {read=pGetAlpha,write=pSetAlpha};
+  __property int FontSize = {read=pGetFontSize,write=pSetFontSize};
+
+  void Line(int,int,int,int);
+  void Rect(int,int,int,int);
+  void DrawEllipse(int,int,int,int);
+  void FillEllipse(int,int,int,int);
+  void DrawCircle(double,double,double);
+  void FillCircle(double,double,double);
+  void FillRect(int,int,int,int);
+  void Draw(TCanvas *,int = 0,int = 0);
+  void Clear(TColor);
+  void Polyline(TPoint *,int);
+  void Polygon(TPoint *,int);
+  void DrawImage(void *,float,float,float,float);
+  void TextOut(double,double,AnsiString);
+  void * GetInternalBitmap();
+
+  ppGdiPlus(int = 100,int = 100); // Constr
+  ~ppGdiPlus();       // Destr
+};
