96273a88d9a39374fca0209a3676a1ca078dd82a
podoroges <pavel.krents@gmail.com>
2018-08-01 15:29:30 +0300

    v1.1

--- ppStr.cpp
+++ ppStr.cpp
@@ -1,5 +1,27 @@
 #include <classes.hpp>
 //---------------------------------------------------------------------------
+// Extract n-th word, starting with '0'
+// If n more than words count, last word returned
+// StrParm("Magic The Gathering",1," ") = "The"
+AnsiString StrParm(AnsiString st,int n,AnsiString sample){
+  while(n>0){
+    st = st.Delete(1,st.Pos(sample)+sample.Length()-1);
+    n--;
+  }
+  st = st.Delete(st.Pos(sample),st.Length()-st.Pos(sample)+1);
+  return st;
+}
+//---------------------------------------------------------------------------
+//  StrCnt("A+B+C","+") = 2
+int StrCnt(AnsiString st,AnsiString sample){
+  int a=0;
+  while(st.Pos(sample)>0){
+    st=st.Delete(st.Pos(sample),sample.Length());
+    a++;
+  }
+  return a;
+}
+//---------------------------------------------------------------------------
 //  ExtractString1("archive1.zip","arc",".zip") = "hive1"
 AnsiString ExtractString1(AnsiString source,AnsiString start,AnsiString end){
   if(!source.Pos(start))return "";
--- ppStr.h
+++ ppStr.h
@@ -1,12 +1,21 @@
 /*
-  Library ppStr v1.0
+  Library ppStr v1.1
 
   by Podoroges
   Kiev, Ukraine
 
+  31/07/2018 v1.1 Added StrCnt, StrParm
   25/06/2018 v1.0 Initial release
-  
+
 */
+
+// Extract n-th word, starting with '0'
+// If n more than words count, last word returned
+// StrParm("Magic The Gathering",1," ") = "The"
+AnsiString StrParm(AnsiString st,int n,AnsiString sample = " ");
+
+//  StrCnt("A+B+C","+") = 2
+int StrCnt(AnsiString st,AnsiString sample = " ");
 
 // ExtractString1("archive1.zip","arc",".zip") = "hive1"
 AnsiString ExtractString1(AnsiString source,AnsiString start,AnsiString end);
