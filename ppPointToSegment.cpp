6ddd4b88965e2d51b8441cca36d4241e3a3cb19f
podoroges <pavel.krents@gmail.com>
2018-12-03 22:08:11 +0200

    1.0

---
+++ ppPointToSegment.cpp
@@ -0,0 +1,27 @@
+#include <math.h>
+
+double sqr(double x){
+  return x*x;
+}
+
+double PointToSegment(double x0,double y0,double x1,double y1,double x2,double y2){
+  // Legs squares
+  double a=sqr(x0-x1)+sqr(y0-y1);
+  double b=sqr(x0-x2)+sqr(y0-y2);
+  // Base square
+  double c=sqr(x2-x1)+sqr(y2-y1);
+  if(c==0){ // If zero base, distance is one of the legs
+    return sqrt(a);
+  }
+  // Trinagle area
+  double s=fabs((x0-x2)*(y1-y2)-(x1-x2)*(y0-y2))/2;
+  // Obtuse triangle check
+  if((a+c<b)||(b+c<a)){
+    if(a<b)
+      return sqrt(a);
+    return sqrt(b);
+  }
+  // Acute triangle - return height above Base
+  return 2*s/sqrt(c); // c is not zero, checked above
+}
+
---
+++ ppPointToSegment.h
@@ -0,0 +1,11 @@
+/*
+  ppPointToSegment v1.0
+
+  by Podoroges
+  Kiev, Ukraine
+
+  03/12/2018 v1.0 Initial release
+*/
+
+double PointToSegment(double x0,double y0,double x1,double y1,double x2,double y2);
+
