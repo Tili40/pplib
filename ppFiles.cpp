fb3a0e27105d7b1ac999a0023011e13e5edf18fb
podoroges <pavel.krents@gmail.com>
2020-03-26 10:01:30 +0100

    v 1.1

--- ppFiles.cpp
+++ ppFiles.cpp
@@ -3,8 +3,10 @@
 AnsiString ppGetFileList(AnsiString path){
 	WIN32_FIND_DATA ffd;
 	HANDLE hFind = INVALID_HANDLE_VALUE;
-	TStringList * str = new TStringList();
 	hFind = FindFirstFile(path.c_str(), &ffd);
+	if(hFind == INVALID_HANDLE_VALUE)
+		return "";
+	TStringList * str = new TStringList();
 	do{
 		if (!(ffd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY))
 			str->Add(ffd.cFileName);
--- ppFiles.h
+++ ppFiles.h
@@ -1,3 +1,13 @@
-AnsiString ppGetFileList(AnsiString path);
+/*
+  Library ppFiles v1.1
 
+  by Podoroges
+  Kiev, Ukraine
+
+  26/03/2020 v1.1 Fixed ppGetFileList - added initial handle check.
+  25/06/2019 v1.0 Initial release
+
+*/
+
+AnsiString ppGetFileList(AnsiString path);
 int ppFileExists(AnsiString fname);
